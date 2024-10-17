eedb08b0d8c0620bc7f056e908da9b04f1988ec4
Carson <carsonjmaurer@gmail.com>
2024-10-17 09:13:34 -0700

    added sword collider

--- The Band Of Gold/Assets/Carson Folder/Sword/Sword.cs
+++ The Band Of Gold/Assets/Carson Folder/Sword/Sword.cs
@@ -6,6 +6,7 @@ public class Sword : MonoBehaviour
 {
     [SerializeField] private GameObject slashAnimPrefab;
     [SerializeField] private Transform slashAnimSpawnPoint;
+    [SerializeField] private Transform weaponCollider;
 
     private PlayerControls playerControls;
     private Animator myAnimator;
@@ -66,8 +67,11 @@ public class Sword : MonoBehaviour
 
         if (mousePos.x < playerScreenPoint.x) {
             activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0, -180, 0 );
         } else {
             activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+            weaponCollider.transform.rotation = Quaternion.Euler(0, 0, 0 );
         }
     }
+
 }
