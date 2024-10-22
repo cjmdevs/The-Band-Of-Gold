d46b7862ebb032d21a4dd5839c3dc44a290a2054
entimii <emoarn427@gmail.com>
2024-10-22 09:19:30 -0700

    x

---
+++ The Band Of Gold/Assets/Scripts/MainMenu/SaveSlot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SaveSlot : MonoBehaviour
+{
+    [Header("Profile")]
+    [SerializeField] private string profileId = "";
+
+    [Header("Content")]
+    [SerializeField] private GameObject noDataConntent;
+    [SerializeField] private GameObject hasDataContent;
+    [SerializeField] private TextMeshProUGUI percentageCompleteText;
+    [SerializeField] private TextMeshProUGUI deathCountText;
+}
