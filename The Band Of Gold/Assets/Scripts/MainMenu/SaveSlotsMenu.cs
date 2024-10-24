5633e51819b3f719bcd0ac749cf1e871e7472534
entimii <emoarn427@gmail.com>
2024-10-24 08:34:18 -0700

    fixed errors in save menu script

--- The Band Of Gold/Assets/Scripts/MainMenu/SaveSlotsMenu.cs
+++ The Band Of Gold/Assets/Scripts/MainMenu/SaveSlotsMenu.cs
@@ -16,17 +16,17 @@ public class SaveSlotsMenu : MonoBehaviour
         ActivateMenu();
     }
 
-    public void ActiveMenu()
+    public void ActivateMenu()
     {
         // Load all of the profiles that exist
         Dictionary<string, GameData> profilesGameData = DataPersistenceManager.instance.GetAllProfilesGameData();
 
         // Loop through eaach saave slot in the UI and set the content appropriately
-        foreach (SaveSlot saaveSlot in saveSlots)
+        foreach (SaveSlot saveSlot in saveSlots)
         {
             GameData profileData = null;
             profilesGameData.TryGetValue(saveSlot.GetProfileId(), out profileData);
-            saaveSlot.SetData(profileData);
+            saveSlot.SetData(profileData);
         }
     }
 }
