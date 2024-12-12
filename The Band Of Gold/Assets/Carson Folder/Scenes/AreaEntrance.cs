c57c5c09a7e867b50e5fbdb4378a045986e74815
entimii <emoarn427@gmail.com>
2024-12-12 09:18:44 -0800

    Enemy follows player, added player health script

--- The Band Of Gold/Assets/Carson Folder/Scenes/AreaEntrance.cs
+++ The Band Of Gold/Assets/Carson Folder/Scenes/AreaEntrance.cs
@@ -7,7 +7,8 @@ public class AreaEntrance : MonoBehaviour
     [SerializeField] private string transitionName;
 
     private void Start() {
-        if (transitionName == SceneManagement.Instance.SceneTransitionName) {
+        if (transitionName == SceneManagement.Instance.SceneTransitionName) 
+        {
             PlayerController.Instance.transform.position = this.transform.position;
         }
     }
