8fc52cf7e967e438dfb02efcfd317ead8f5e38c2
Carson Maurer <carsonjmaurer@gmail.com>
2025-01-16 08:46:29 -0800

    working on adding cooldown

--- The Band Of Gold/Assets/Carson Folder/Enemy/enemy_movement.cs
+++ The Band Of Gold/Assets/Carson Folder/Enemy/enemy_movement.cs
@@ -8,13 +8,21 @@ public class enemy_movement : MonoBehaviour
 {   
 
     public float speed;
-    private EnemyState enemyState;
-
+    public float playerDetectRange = 5;
+    public Transform detectionPoint;
+    public LayerMask playerLayer;
     public float attackRange = 2;
-    public Rigidbody2D rb;
+    public float attackCooldown = 2;
+
+
+    private EnemyState enemyState;
     private Transform player;
     private int facingDirection = -1;
     private Animator anim;
+    private float attackCooldownTimer;
+
+    private Rigidbody2D rb;
+
     void Start()
     {
         
@@ -26,6 +34,10 @@ public class enemy_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckForPlayer();
+        if(attackCooldownTimer > 0){
+            attackCooldownTimer -= Time.deltaTime;
+        }
         if(enemyState == EnemyState.Chasing){
             Chase();
         }
@@ -36,11 +48,8 @@ public class enemy_movement : MonoBehaviour
     }
 
     void Chase(){
-        if(Vector2.Distance(transform.position, player.transform.position) <= attackRange)
-        {
-            ChangeState(EnemyState.Attacking);
-        }
-        else if(player.position.x > transform.position.x && facingDirection == -1 || player.position.x < transform.position.x && facingDirection == 1){
+
+        if(player.position.x > transform.position.x && facingDirection == -1 || player.position.x < transform.position.x && facingDirection == 1){
             Flip();
         }
         Vector2 direction = (player.position - transform.position).normalized;
@@ -52,55 +61,72 @@ public class enemy_movement : MonoBehaviour
         facingDirection *= -1;
         transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
-    private void OnTriggerStay2D(Collider2D collision){
-        if (collision.gameObject.tag == "Player"){
-            if (player == null) {
-                player = collision.transform;
-            }
-            
 
-            ChangeState(EnemyState.Chasing);
-        }
-        
-    }
-    private void OnTriggerExit2D(Collider2D collision){
-         if (collision.gameObject.tag == "Player"){
-            rb.velocity = Vector2.zero;
 
-            ChangeState(EnemyState.Idle);
-        }
-    }
-    void ChangeState(EnemyState newState){
-        //exit current animation
-        if(enemyState == EnemyState.Idle){
-            anim.SetBool("isIdle", false);
-
-        }
-        else if (enemyState == EnemyState.Chasing){
-            anim.SetBool("isChasing", false);
-        }
-        else if (enemyState == EnemyState.Attacking){
-            anim.SetBool("isAttacking", false);
-        }
-
-        enemyState = newState;
-
-        //update new animations
-        if(enemyState == EnemyState.Idle){
-            anim.SetBool("isIdle", true);
-
-        }
-        else if (enemyState == EnemyState.Chasing){
-            anim.SetBool("isChasing", true);
-        }
-        else if (enemyState == EnemyState.Attacking){
-            anim.SetBool("isAttacking", true);
-        }
-    }
-    public void Attack()
+    private void CheckForPlayer()
     {
-        Debug.Log("Attacking Player");
+        Collider2D[] hits = Physics2D.OverlapCircleAll(detectionPoint.position, playerDetectRange, playerLayer);
+
+        if(hits.Length > 0)
+        {
+
+            player = hits[0].transform;
+
+            if (Vector2.Distance(transform.position, player.transform.position) <= attackRange && attackCooldownTimer <= 0)
+            {
+   
+                attackCooldownTimer = attackCooldown;
+                ChangeState(EnemyState.Attacking);
+
+            }
+            else if(Vector2.Distance(transform.position, player.position) > attackRange)
+            {
+
+                ChangeState(EnemyState.Chasing);
+            }
+        else
+        {
+            ChangeState(EnemyState.Idle);
+            rb.velocity = Vector2.zero;
+        }
+            
+        }   
     }
+        
+
+    void ChangeState(EnemyState newState){
+    //exit current animation
+    if(enemyState == EnemyState.Idle){
+        anim.SetBool("isIdle", false);
+        Debug.Log("Exiting Idle State");
+    }
+    else if (enemyState == EnemyState.Chasing){
+        anim.SetBool("isChasing", false);
+        Debug.Log("Exiting Chasing State");
+    }
+    else if (enemyState == EnemyState.Attacking){
+        anim.SetBool("isAttacking", false);
+        Debug.Log("Exiting Attacking State");
+    }
+
+    enemyState = newState;
+    Debug.Log("Changing State to: " + newState);
+
+    //update new animations
+    if(enemyState == EnemyState.Idle){
+        anim.SetBool("isIdle", true);
+        Debug.Log("Entering Idle State");
+    }
+    else if (enemyState == EnemyState.Chasing){
+        anim.SetBool("isChasing", true);
+        Debug.Log("Entering Chasing State");
+    }
+    else if (enemyState == EnemyState.Attacking){
+        Debug.Log("Attacking Player");
+        anim.SetBool("isAttacking", true);
+    }
+}
+
 }
 
 public enum EnemyState{
