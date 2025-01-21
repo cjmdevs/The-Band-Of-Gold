ef6f1b0b39cb042cc09fa1fcf5a6687d36de0b69
Carson <carsonjmaurer@gmail.com>
2025-01-21 09:11:31 -0800

    fixed bugs

--- The Band Of Gold/Assets/Carson Folder/Sword/Sword.cs
+++ The Band Of Gold/Assets/Carson Folder/Sword/Sword.cs
@@ -55,6 +55,7 @@ public class Sword : MonoBehaviour
             slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity);
             slashAnim.transform.parent = this.transform.parent;
             StartCoroutine(AttackCDRoutine());
+            
         }
     }
 
