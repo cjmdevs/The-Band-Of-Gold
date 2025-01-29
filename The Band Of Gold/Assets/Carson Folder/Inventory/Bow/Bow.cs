4f34ef9f4a8ab21693a7f18dc26d596023932ed2
Carson <carsonjmaurer@gmail.com>
2025-01-29 09:09:21 -0800

    added bow damage

--- The Band Of Gold/Assets/Carson Folder/Inventory/Bow/Bow.cs
+++ The Band Of Gold/Assets/Carson Folder/Inventory/Bow/Bow.cs
@@ -21,7 +21,9 @@ public class Bow: MonoBehaviour, IWeapon
     {
         myAnimator.SetTrigger(FIRE_HASH);
         GameObject newArrow = Instantiate(arrowPrefab, arrowSpawnPoint.position, ActiveWeapon.Instance.transform.rotation);
+        newArrow.GetComponent<Projectile>().UpdateWeaponInfo(weaponInfo);
     }
+
 
     public WeaponInfo GetWeaponInfo()
     {
