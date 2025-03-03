d2107dee6b85b5a5644a77a3ff120caf7aacd2e1
entimii <emoarn427@gmail.com>
2025-03-03 09:25:42 -0800

    Main Menu Animation

--- The Band Of Gold/Assets/ClassWeaponsManager.cs
+++ The Band Of Gold/Assets/ClassWeaponsManager.cs
@@ -15,18 +15,21 @@ public class ClassWeaponsManager : MonoBehaviour
     [SerializeField] private GameObject Sword;
     [SerializeField] private GameObject SwordPrefab;
     [SerializeField] private GameObject SwordInventory;
+    [SerializeField] private GameObject SwordEnabled;
 
     // Staff Subheader
     [Header("Mage's Staff")]
     [SerializeField] private GameObject Staff;
     [SerializeField] private GameObject StaffPrefab;
     [SerializeField] private GameObject StaffInventory;
+    [SerializeField] private GameObject StaffEnabled;
 
     // Bow Subheader
     [Header("Archer's Bow")]
     [SerializeField] private GameObject Bow;
     [SerializeField] private GameObject BowPrefab;
     [SerializeField] private GameObject BowInventory;
+    [SerializeField] private GameObject BowEnabled;
 
     // Loaded Character
     [Header("Loaded Character")]
@@ -39,16 +42,19 @@ public class ClassWeaponsManager : MonoBehaviour
         Sword.SetActive(false);
         SwordPrefab.SetActive(false);
         SwordInventory.SetActive(false);
+        SwordEnabled.SetActive(false);
 
         // Staff
         Staff.SetActive(false);
         StaffPrefab.SetActive(false);
         StaffInventory.SetActive(false);
+        StaffEnabled.SetActive(false);
 
         // Bow
         Bow.SetActive(false);
         BowPrefab.SetActive(false);
         BowInventory.SetActive(false);
+        BowEnabled.SetActive(false);
 
         LoadCharacter();
     }
@@ -61,16 +67,19 @@ public class ClassWeaponsManager : MonoBehaviour
                 Sword.SetActive(true);
                 SwordPrefab.SetActive(true);
                 SwordInventory.SetActive(true);
+                SwordEnabled.SetActive(true);
                 break;
             case "Mage":
                 Staff.SetActive(true);
                 StaffPrefab.SetActive(true);
                 StaffInventory.SetActive(true);
+                StaffEnabled.SetActive(true);
                 break;
             case "Archer":
                 Bow.SetActive(true);
                 BowPrefab.SetActive(true);
                 BowInventory.SetActive(true);
+                BowEnabled.SetActive(true);
                 break;
         }
     }
