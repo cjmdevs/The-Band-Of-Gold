812d0e99b825181711c417a4e854f2d5a10a61b7
Carson <carsonjmaurer@gmail.com>
2025-03-04 09:13:35 -0800

    added skeleton

--- The Band Of Gold/Assets/Carson Folder/Enemy/enemy_combat.cs
+++ The Band Of Gold/Assets/Carson Folder/Enemy/enemy_combat.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class enemy_combat : MonoBehaviour
 {
-    
-    public int damage = 1; // damage enemy will do
-    public float KnockbackForce; // amount of knockback enemy does
-    public float stunTime; // amount of time player is stunned for
+    public int damage = 1;
+    public float KnockbackForce;
+    public float stunTime;
+
+    // Optional: For sword-like attacks
     public Transform attackPoint;
     public float weaponRange;
+    public bool useSwordAttack = false; // Flag to switch between attack types
+
     public LayerMask playerLayer;
 
     AudioManager audioManager;
@@ -19,16 +22,53 @@ public class enemy_combat : MonoBehaviour
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
     }
 
+    public void EnemyAttack()
+    {
+        if (useSwordAttack)
+        {
+            SwordAttack();
+        }
+        else
+        {
+            TouchAttack();
+        }
+    }
 
-    public void EnemyAttack(){
+    private void SwordAttack()
+    {
         Collider2D[] hits = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, playerLayer);
 
-        if(hits.Length > 0)
+        if (hits.Length > 0)
         {
             audioManager.PlaySFX(audioManager.playerHit);
             hits[0].GetComponent<PlayerHealth>().ChangeHealth(-damage);
             hits[0].GetComponent<PlayerController>().Knockback(transform, KnockbackForce, stunTime);
-            Debug.Log("Attack");
+            Debug.Log("Sword Attack");
+        }
+    }
+
+    private void TouchAttack()
+    {
+        // Damage is applied in OnTriggerEnter2D
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!useSwordAttack && (playerLayer.value & (1 << collision.gameObject.layer)) != 0) //check if the collided object is on the player layer.
+        {
+            audioManager.PlaySFX(audioManager.playerHit);
+            collision.GetComponent<PlayerHealth>().ChangeHealth(-damage);
+            collision.GetComponent<PlayerController>().Knockback(transform, KnockbackForce, stunTime);
+            Debug.Log("Touch Attack");
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (useSwordAttack && attackPoint != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(attackPoint.position, weaponRange);
         }
     }
 }
